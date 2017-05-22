23171cb1a1ca7c8ccb34229d0a5adc2794d04484
Bruno Casali <brunoocasali@gmail.com>
2017-05-22 10:58:40 -0300

    Update layout, texts and flow based on comment above
    https://www.chatwork.com/#!rid20430558-2020200902

--- index.js
+++ index.js
@@ -24,16 +24,19 @@ function getFormData(){
 }
 
 function getText(data){
-    var result = [];
-
-    if (data.is_product) result.push("Produto");
-    if (data.is_store) result.push("Loja");
-
-    return result.join(' e ');  
+    if (data.is_product && !data.is_store) 
+        return "Ótimo! Esse é um review somente de produto!";
+    else 
+        return "Oops! Agora você deve escrever apenas sobre o produto. " +
+               "Você vai poder falar sobre a loja depois.";
 }
 
 // Make the actual CORS request.
 function checkReview() {
+  $('#result').text('');
+  $('#error').text('');
+  $('#comment-text').removeClass('error');
+
   // This is a sample server that supports CORS.
   var url = 'https://rambo-trustvox.herokuapp.com/sentiment/comments/';
 
@@ -48,9 +51,16 @@ function checkReview() {
   xhr.onload = function() {
     var data = JSON.parse(xhr.responseText);
     console.log(data);
-    
-    $('#result').text(getText(data));
-    $('.box').removeClass('hide');
+
+    if (data.comment.constructor === Array) {
+        $('#comment-text').addClass('error');
+        $('#error').text(data.comment.join(' / '));
+        $('#result-box').addClass('hide');
+    }
+    else {
+        $('#result').text(getText(data));
+        $('#result-box').removeClass('hide');
+    }
   };
 
   xhr.onerror = function() {
