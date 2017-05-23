e6563872d9f3d505f28eb98aefb9fd7b6d7aad63
Bruno Casali <brunoocasali@gmail.com>
2017-05-23 09:31:08 -0300

    Use Ajax instead of creating a XML request object

--- index.js
+++ index.js
@@ -1,28 +1,3 @@
-// Create the XHR object.
-function createCORSRequest(method, url) {
-  var xhr = new XMLHttpRequest();
-  if ("withCredentials" in xhr) {
-    // XHR for Chrome/Firefox/Opera/Safari.
-    xhr.open(method, url, true);
-  } else if (typeof XDomainRequest != "undefined") {
-    // XDomainRequest for IE.
-    xhr = new XDomainRequest();
-    xhr.open(method, url);
-  } else {
-    // CORS not supported.
-    xhr = null;
-  }
-
-  return xhr;
-}
-
-function getFormData(){
-    var data = new FormData();
-    data.append('comment', $('#comment-text').val());
-
-    return data;
-}
-
 function getText(data){
     if (data.is_product && !data.is_store) 
         return "Ótimo! Esse é um review somente de produto!";
@@ -31,41 +6,30 @@ function getText(data){
                "Você vai poder falar sobre a loja depois.";
 }
 
-// Make the actual CORS request.
 function checkReview() {
-  $('#result').text('...');
-  $('#error').text('');
-  $('#comment-text').removeClass('error');
+    $('#result').text('...');
+    $('#error').text('');
+    $('#comment-text').removeClass('error');
 
-  // This is a sample server that supports CORS.
-  var url = 'https://rambo-trustvox.herokuapp.com/sentiment/comments/';
+    $.ajax({
+        type: "POST",
+        data: JSON.stringify({ 
+            "comment": $('#comment-text').val() 
+        }),
+        url: 'https://rambo-trustvox.herokuapp.com/sentiment/comments/',
+        contentType: "application/json; charset=utf-8",
+        dataType: "JSON",
+        processData: true,
+        success: function (data) {
+            console.log(data);
 
-  var xhr = createCORSRequest('POST', url);
-  if (!xhr) {
-    console.log('CORS not supported');
-
-    return;
-  }
-
-  // Response handlers.
-  xhr.onload = function() {
-    var data = JSON.parse(xhr.responseText);
-    console.log(data);
-
-    if (data.comment.constructor === Array) {
-        $('#comment-text').addClass('error');
-        $('#error').text(data.comment.join(' / '));
-        $('#result-box').addClass('hide');
-    }
-    else {
-        $('#result').text(getText(data));
-        $('#result-box').removeClass('hide');
-    }
-  };
-
-  xhr.onerror = function() {
-    console.err('Woops, there was an error making the request.');
-  };
-
-  xhr.send(getFormData());
+            $('#result').text(getText(data));
+            $('#result-box').removeClass('hide');
+        },
+        error: function (data) {
+            $('#comment-text').addClass('error');
+            $('#error').text(data.responseJSON.comment.join(' / '));
+            $('#result-box').addClass('hide');
+        }
+    });
 }
