153c748709607036c18f7fb2389e9d488f927508
diegothuran <diego.thuran@gmail.com>
2017-08-24 16:08:42 -0300

    - Layout Changing

--- index.js
+++ index.js
@@ -22,13 +22,14 @@ function checkReview() {
         processData: true,
         success: function (data) {
             console.log(data);
+            result = getText(data);
 
-            $('#result').text(getText(data));
+            $('#result').text(result);
             $('#result-box').removeClass('hide');
-            if(data.is_store)
-                $('#result').addColors('#ebd1d1')
+            if(result == "Esse review contém comentários sobre loja, atendimento ou entrega.")
+                $('#result').style().color = '#ebd1d1';
             else
-                $('#result').addColors('#d6f0cf')
+                $('#result').style().color ='#d6f0cf';
         },
         error: function (data) {
             $('#comment-text').addClass('error');
